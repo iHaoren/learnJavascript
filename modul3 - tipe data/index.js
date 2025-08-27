f0c5c32ca3a36c78ce5de4e74507e474ea73137e
Ibnu Harun <ibnuharunalmudzakir@gmail.com>
2025-08-27 11:41:05 +0700

    first commit

---
+++ modul3 - tipe data/index.js
@@ -0,0 +1,39 @@
+// String
+const firstName = "Ibnu";
+const lastName = `${firstName} Harun`;
+const suku = "Jawa";
+
+console.log(lastName, typeof lastName)
+
+// Number
+const umur = 19.2
+
+console.log(umur, typeof umur)
+
+// Boolean
+const isMarried = true
+
+console.log(isMarried, typeof isMarried)
+
+// Undifined
+let x;
+
+console.log(x, typeof x)
+
+// null
+const person = null
+
+console.log(person, typeof person)
+
+// Symbol
+const symbol1 = Symbol("description 1")
+
+console.log(symbol1, typeof symbol1)
+
+// 
+
+
+
+const output = symbol1
+
+console.log(output, typeof output)
